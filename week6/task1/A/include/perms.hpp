6f65c27f763e3495dfa37742ffb3b999792613bd
Aleksei12082001 <arapov-alesha.arapov2015@yandex.ru>
2025-01-16 18:08:01 +0300

    Удалён подмодуль week6

--- week6/task1/A/include/perms.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef PERMS_HPP
-#define PERMS_HPP
-
-int** perms(int* nums, int size, int& totalPerms);
-
-#endif 
-
--- week6/task1/B/include/maxprofit.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef NUMBER_HPP
-#define NUMBER_HPP
-
-int maxProfit(int prices[], int size);
-
-#endif 
-
-
